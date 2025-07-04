138405b51ccc29f86a69a746640f2508014d4d92
PNP <fathahpnp@gmail.com>
2025-07-04 17:01:06 +0530

    first commit

---
+++ JS/Js.js
@@ -0,0 +1,14 @@
+$("document").ready(function() {
+    // Function to handle the click event on the button
+    $(".resume").click(function() {
+        alert("This function is note completed yet!");
+
+        alert("It will be completed as soon as possible\. Sorry for the inconvenience.");
+    });
+    $(".blogs").click(function() {
+        alert("This function is note completed yet!");
+        alert("Blogs are coming soon!");
+    });
+
+
+});
